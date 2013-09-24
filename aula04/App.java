c79af742a6a2142f8a177736650d713a049aaa01
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2013-09-24 13:05:47 +0100

    aula 04

---
+++ aula04/App.java
@@ -0,0 +1,15 @@
+import java.util.Calendar;
+
+class App{
+
+  void myFunction1(){
+    System.out.println("Ola");
+	System.out.println("1");
+	System.out.println("2");
+  }
+  void myFunction2(){
+    System.out.println("3");
+	System.out.println("4");
+  }
+
+}
