1de0cdd0ead93c3b944298d04b0c240a32e11383
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2013-11-29 15:41:40 +0000

    Aula 34: esqueleto das classes Io e ArraysUtils

---
+++ aula34/ArraysUtils.java
@@ -0,0 +1,45 @@
+public class ArraysUtils{
+	/*
+	 * Join
+	 */
+	static void join(int [] src1, int [] src2, int[] res){
+	
+	}
+	/*
+	 * Sort array in descending order
+	 */
+	static void sortDesc(int [] src, int[] res){
+	}
+	/*
+	 * Sort array in ascending order
+	 */
+	static void sortAsc(int [] src, int[] res){
+	}
+	static int indexOfMinElem(int [] arr){
+		return 0;
+	}
+	
+	static int indexOfMaxElem(int [] arr){
+		return 0;
+	}
+
+	static int [] cloneArray(int [] a){
+		return new int[a.length];
+	}
+
+	/*
+	 * Retorna o numero de ocurrencias de nr no array a
+	 */
+	static int occurrences(int [] a, int nr){
+		return -1;
+	}
+
+	/*
+	 * Retorna um novo array de tamanho igual a capacity e
+	 * inicializado aleatoriamente com elementos contidos no 
+	 * intervalo [0, max]
+	 */
+	static int[] newRandArray(int capacity, int max){
+		return new int[capacity];
+	}
+}
